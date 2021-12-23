76d03042da7f0df3d8d7dc3663337219b6ea5024
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-12-23 23:05:30 +0530

    sett

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Youtube from 'react-youtube'
 import './RowPost.css';
 import {useState,useEffect} from 'react'
 import axios from '../../axios'
-import {imageUrl} from '../../constants/constants'
+import {imageUrl,API_KEY} from '../../constants/constants'
+
 
 
 function RowPost(props) {
     const [movies, setMovies] = useState([])
+    const [urlId, setId] = useState('')
     useEffect(() => {
        axios.get(props.url).then((response)=>{
            console.log(response.data)
@@ -15,17 +18,37 @@ function RowPost(props) {
         //    alert("error")
        })
     }, [])
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 0,
+        },
+      };
+      const handleMovie=(id)=>{
+            console.log(id)
+            axios.get(`/movie/634649/videos?api_key=${API_KEY}`).then(response=>{
+                if(response.data.results.length!==0){
+                    setId(response.data.results[0])
+                }else{
+                    console.log("Trailer not found")
+                }
+                
+            })
+      }
     return (
         <div className='row'>
             <h2>{props.title}</h2>
             <div className='posters'>
                 {movies.map((obj)=>
-                      <img className={props.isSmall?'smallPoster':'poster' }src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+                      <img onClick={()=>handleMovie(obj.id)} className={props.isSmall?'smallPoster':'poster' }src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
                  
                 )}
                  
                  
             </div>
+          { urlId && <Youtube videoId={urlId.key} opts={opts}/>   }  
         </div>
     )
 }
